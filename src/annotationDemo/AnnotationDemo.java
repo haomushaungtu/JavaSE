2048e1571190bd259e4519855f3c0f59ff000bc6
huanghao <1303219245@qq.com>
2018-11-29 22:42:32 +0800

    add annotationDemo

---
+++ src/annotationDemo/AnnotationDemo.java
@@ -0,0 +1,21 @@
+package annotationDemo;
+
+import java.lang.reflect.Field;
+
+public class AnnotationDemo {
+    @FirstAnnotation(height = 100)
+    private String height;
+
+    public static void main(String[] args) {
+        AnnotationDemo demo = new AnnotationDemo();
+        Class<? extends AnnotationDemo> clazz = demo.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(FirstAnnotation.class)) {
+                FirstAnnotation annotation = field.getAnnotation(FirstAnnotation.class);
+                System.out.println(annotation.height());
+            }
+            System.out.println(field.getName());
+        }
+    }
+}
