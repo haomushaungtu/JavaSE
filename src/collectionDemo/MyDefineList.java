0227bdb3812495c7f3c15e6c044e090280fed1e2
huanghao <1303219245@qq.com>
2019-05-13 18:27:26 +0800

    define a list collection

---
+++ src/collectionDemo/MyDefineList.java
@@ -0,0 +1,24 @@
+package collectionDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Collection 接口List
+ * 
+ * @author huanghao
+ *
+ */
+public class MyDefineList {
+    private List<String> upperList = new ArrayList<>();
+    
+    public void copyListMethod() {
+        upperList.add("1");
+        System.out.println(upperList.isEmpty());
+    }
+    
+    public static void main(String[] args) {
+        MyDefineList defineList = new MyDefineList();
+        defineList.copyListMethod();
+    }
+}
