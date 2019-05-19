66877fdbc240541c92fd6984061b2ff7192ce196
huanghao <1303219245@qq.com>
2019-05-19 17:44:04 +0800

    define a arrayList

--- src/collectionDemo/DefineArrayList.java
+++ src/collectionDemo/DefineArrayList.java
@@ -1,6 +1,7 @@
 package collectionDemo;
 
 import java.util.AbstractList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.RandomAccess;
 
@@ -8,6 +9,22 @@ public class DefineArrayList<E> extends AbstractList<E>
         implements List<E>, RandomAccess, Cloneable, java.io.Serializable {
 
     private static final long serialVersionUID = 4409460913836361707L;
+
+    /**
+     * 容器数组
+     */
+    Object[] elementData;
+
+    /**
+     * 元素个数
+     */
+    private int size;
+
+    /**
+     * 修改次数
+     */
+    private int modCount;
+
     /**
      * 默认初始化值
      */
@@ -16,15 +33,112 @@ public class DefineArrayList<E> extends AbstractList<E>
     /**
      * 空数组实例
      */
+    @SuppressWarnings("unused")
     private static final Object[] EMPTY_ELEMENTDATA = {};
+
+    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+
+    public DefineArrayList() {
+        this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+    }
+
+    public boolean add(E e) {
+        ensureCapacityInternal(size + 1); // Increments modCount!!
+        elementData[size++] = e;
+        return true;
+    }
 
     @Override
     public E get(int index) {
-        return null;
+        rangeCheck(index);
+        return elementData(index);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void ensureCapacityInternal(int minCapacity) {
+        ensureExplicitCapacity(calculateCapacity(elementData, minCapacity));
+    }
+
+    /**
+     * 计算目前需要容量
+     * 
+     * @author huangaho
+     * @param elementData
+     * @param minCapacity
+     * @return
+     */
+    private static int calculateCapacity(Object[] elementData, int minCapacity) {
+        // 扩充容量小于默认值时，容量大小最小为默认值
+        if (elementData == DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
+            return Math.max(minCapacity, DEFAULT_CAPACITY);
+        }
+        return minCapacity;
+    }
+
+    /**
+     * 
+     * @author huangaho
+     * @params
+     * @param minCapacity
+     */
+    public void ensureExplicitCapacity(int minCapacity) {
+        modCount++;
+        // overflow-conscious code
+        if (minCapacity - elementData.length > 0)
+            grow(minCapacity);
+    }
+
+    /**
+     * 扩容
+     * 
+     * @author huangaho
+     * @params
+     * @param minCapacity
+     */
+    private void grow(int minCapacity) {
+        int oldCapacity = elementData.length;
+        // 新数组容量为旧数组的1.5倍
+        int newCapacity = oldCapacity + oldCapacity >> 1;
+        if (newCapacity - minCapacity < 0) {
+            newCapacity = minCapacity;
+        }
+        if (newCapacity - MAX_ARRAY_SIZE > 0) {
+            newCapacity = hugeCapacity(minCapacity);
+        }
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+
+    /**
+     * 扩容超过一定值时，给予新增最大值数组
+     * 
+     * @author huangaho
+     * @params
+     * @param minCapacity
+     * @return
+     */
+    private static int hugeCapacity(int minCapacity) {
+        if (minCapacity < 0) // overflow
+            throw new OutOfMemoryError();
+        return (minCapacity > MAX_ARRAY_SIZE) ? Integer.MAX_VALUE : MAX_ARRAY_SIZE;
+    }
+
+    private void rangeCheck(int index) {
+        if (index >= size)
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+    }
+
+    private String outOfBoundsMsg(int index) {
+        return "Index: " + index + ", Size: " + size;
+    }
+
+    @SuppressWarnings("unchecked")
+    E elementData(int index) {
+        return (E) elementData[index];
     }
 }
--- src/collectionDemo/MyDefineList.java
+++ src/collectionDemo/MyDefineList.java
@@ -11,14 +11,17 @@ import java.util.List;
  */
 public class MyDefineList {
     private List<String> upperList = new ArrayList<>();
-    
+
     public void copyListMethod() {
         upperList.add("1");
         System.out.println(upperList.isEmpty());
     }
-    
+
     public static void main(String[] args) {
         MyDefineList defineList = new MyDefineList();
         defineList.copyListMethod();
+        List<String> list = new DefineArrayList<>();
+        list.add("01010");
+        System.out.println(list.toString());
     }
 }
