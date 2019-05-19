1c1940189050839ad52d0e217693f33dac37705f
huanghao <1303219245@qq.com>
2019-05-19 22:57:48 +0800

    add method

--- src/collectionDemo/DefineArrayList.java
+++ src/collectionDemo/DefineArrayList.java
@@ -2,9 +2,20 @@ package collectionDemo;
 
 import java.util.AbstractList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.RandomAccess;
 
+/**
+ * ArrayList 实现原理 @see ArrayList <br>
+ * 1.底层原理为数组数据结构 elementData <br>
+ * 2.添加元素方法原理:(1)计算当前需要扩充数组是否小于默认数组长度10,小于则直接扩充到10数组长度<br>
+ * (2)
+ * 
+ * @author huanghao
+ *
+ * @param <E>
+ */
 public class DefineArrayList<E> extends AbstractList<E>
         implements List<E>, RandomAccess, Cloneable, java.io.Serializable {
 
@@ -36,14 +47,37 @@ public class DefineArrayList<E> extends AbstractList<E>
     @SuppressWarnings("unused")
     private static final Object[] EMPTY_ELEMENTDATA = {};
 
+    /** 空元素数组 */
     private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
 
+    /** 最大数组长度 */
     private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
 
+    /**
+     * 1.7以前默认长度为10，现默认为空元素数组
+     */
     public DefineArrayList() {
         this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
     }
 
+    /**
+     * 指定数组长度的构造方法
+     * 
+     * @param initialCapacity
+     */
+    public DefineArrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.elementData = new Object[initialCapacity];
+        } else if (initialCapacity == 0) {
+            this.elementData = EMPTY_ELEMENTDATA;
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+        }
+    }
+
+    /**
+     * 添加元素
+     */
     public boolean add(E e) {
         ensureCapacityInternal(size + 1); // Increments modCount!!
         elementData[size++] = e;
@@ -61,6 +95,11 @@ public class DefineArrayList<E> extends AbstractList<E>
         return size;
     }
 
+    /**
+     * 扩充数组长度
+     * 
+     * @param minCapacity
+     */
     private void ensureCapacityInternal(int minCapacity) {
         ensureExplicitCapacity(calculateCapacity(elementData, minCapacity));
     }
@@ -128,11 +167,22 @@ public class DefineArrayList<E> extends AbstractList<E>
         return (minCapacity > MAX_ARRAY_SIZE) ? Integer.MAX_VALUE : MAX_ARRAY_SIZE;
     }
 
+    /**
+     * 判断当前索引下标是否超过数组大小
+     * 
+     * @param index
+     */
     private void rangeCheck(int index) {
         if (index >= size)
             throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
     }
 
+    /**
+     * 越界信息
+     * 
+     * @param index
+     * @return
+     */
     private String outOfBoundsMsg(int index) {
         return "Index: " + index + ", Size: " + size;
     }
@@ -140,5 +190,43 @@ public class DefineArrayList<E> extends AbstractList<E>
     @SuppressWarnings("unchecked")
     E elementData(int index) {
         return (E) elementData[index];
+    }
+
+    /**
+     * 移除元素
+     */
+    public E remove(int index) {
+        rangeCheck(index);
+        modCount++;
+        E oldValue = elementData(index);
+        int numMoved = size - index - 1;
+        if (numMoved > 0)
+            System.arraycopy(elementData, index + 1, elementData, index, numMoved);
+        elementData[--size] = null; // clear to let GC do its work
+        return oldValue;
+    }
+
+    /**
+     * 添加整个集合
+     */
+    public boolean addAll(int index, Collection<? extends E> c) {
+        rangeCheckForAdd(index);
+        Object[] a = c.toArray();
+        int numNew = a.length;
+        ensureCapacityInternal(size + numNew); // Increments modCount
+        int numMoved = size - index;
+        if (numMoved > 0)
+            System.arraycopy(elementData, index, elementData, index + numNew, numMoved);
+        System.arraycopy(a, 0, elementData, index, numNew);
+        size += numNew;
+        return numNew != 0;
+    }
+
+    /**
+     * A version of rangeCheck used by add and addAll.
+     */
+    private void rangeCheckForAdd(int index) {
+        if (index > size || index < 0)
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
     }
 }
