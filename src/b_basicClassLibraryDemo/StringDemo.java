df611e4d6e752294ea8e14881c3d60a4e2f8de39
huanghao <1303219245@qq.com>
2019-09-10 11:45:48 +0800

    some changes about system-class

--- src/b_basicClassLibraryDemo/StringDemo.java
+++ src/b_basicClassLibraryDemo/StringDemo.java
@@ -3,17 +3,44 @@ package b_basicClassLibraryDemo;
 /**
  * String类常用类 </br>
  * 1.不可改变类，一旦创建不可改变</br>
+ * 2.由于是不可变类，hashcode值一经创建便缓存起来,不需要重新创建,作为hashmap的键,处理速度很快</br>
+ * 3.final的String类实现字符串池,不可变的字符串常量都可以指向同一个字符串，节省heap空间</br>
+ * 4.不可变类的String实现了多线程安全,同一个字符串实例可以被多个线程共享</br>
  * 
  * @see String
  * @author huanghao
  *
  */
 public class StringDemo {
-    
-    public static void main(String[] args) {
-        String str = "Java";
-        str = str + "class";
-        System.out.println(str);
+
+    private final char value[];
+
+    public StringDemo(char v[]) {
+        this.value = v;
     }
 
+    public void getSameObject() {
+        value[0] = '2';
+        String zeroString = "abc";
+        System.out.println(zeroString.intern());
+        String zero2String = "abc";
+        String oneString = new String("abc");
+        String twoString = new String("abc");
+        String threeString = new String("abcd");
+        System.out.println(oneString.equals(twoString));// true
+        System.out.println(oneString == twoString);// false
+        System.out.println(oneString == zeroString);// false
+        System.out.println(zero2String == zeroString);// true
+        oneString = threeString;
+        System.out.println(oneString);
+        System.out.println(zeroString.codePointAt(1));
+    }
+
+    public static void main(String[] args) {
+        char v[] = { 'a' };
+        StringDemo demo = new StringDemo(v);
+        System.out.println(demo.value);
+        demo.getSameObject();
+        System.out.println(demo.value);
+    }
 }
--- src/b_basicClassLibraryDemo/SystemDemo.java
+++ src/b_basicClassLibraryDemo/SystemDemo.java
@@ -1,9 +1,15 @@
 package b_basicClassLibraryDemo;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
+
+import org.apache.commons.configuration2.XMLConfiguration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 
 /**
  * System类的应用</br>
@@ -14,10 +20,23 @@ import java.util.Properties;
  *
  */
 public class SystemDemo {
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+
+    public void getMessage() throws ConfigurationException {
+        Configurations config = new Configurations();
+        XMLConfiguration xmlConfig = new XMLConfiguration();
+        xmlConfig = config.xml("src/b_basicClassLibraryDemo/jdbc.xml");
+        String abs = xmlConfig.getString("db_user");
+        System.out.println(abs);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, IOException, ConfigurationException {
         Properties prps = System.getProperties();
         prps.store(new FileOutputStream("prps.txt"), "System propreties");
+        FileInputStream ins = new FileInputStream(new File("src/b_basicClassLibraryDemo/jdbc.xml"));
+        prps.loadFromXML(ins);
         System.out.println(System.getProperty("os.name"));
         System.out.println(System.currentTimeMillis());
+        SystemDemo demo = new SystemDemo();
+        demo.getMessage();
     }
 }
